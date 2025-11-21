c7d7d3a356ec7d281ec2bbc1a7e0573b4f5d476f
Victory Ndu <victor5ndu@gmail.com>
2025-11-21 11:07:47 +0100

    integrate web socket connection using a chat example

--- chapter-6/src/app.js
+++ chapter-6/src/app.js
@@ -1,16 +1,49 @@
 import express from "express";
-
 import connectToDB from "./database/db.js";
-import ProductRoutes from "./routes/product.route.js"
+import ProductRoutes from "./routes/product.route.js";
+import { Server } from "socket.io";
+import http from "http";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
+const users = new Map();
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("join", (userName) => {
+    users.set(socket.id, userName);
+
+    io.emit("userJoined", userName);
+
+    io.emit("userList", [...users.values()]);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+
+    users.delete(socket.id);
+
+    io.emit("userList", [...users.values()]);
+  });
+});
+
+app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); //parses form data into json object
-
+app.use(express.urlencoded({ extended: true }));
 app.use("/products", ProductRoutes);
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 connectToDB();
 
-export default app;
+export { server };
--- chapter-6/src/server.js
+++ chapter-6/src/server.js
@@ -1,4 +1,7 @@
-import app from "./app.js"
+import { server } from "./app.js";
+
 const PORT = process.env.PORT || 8383;
 
-app.listen(PORT, () => console.log(`Port Running on ${PORT}`));
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
